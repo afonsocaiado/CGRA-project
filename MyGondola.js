58b9b9d5d601cdce143053126a32086677fd1a90
Afonso Caiado <afonsocaiado1@hotmail.com>
2020-04-23 12:41:59 +0100

    Gondola sem as helices

---
+++ MyGondola.js
@@ -0,0 +1,61 @@
+/**
+ * MyGondola
+ * @constructor
+ * @param scene - Reference to MyScene object
+ */
+class MyGondola extends CGFobject {
+	constructor(scene) {
+		super(scene);
+        this.cilinder = new MyCilinder(this.scene, 16);
+        this.sphere = new MySphere(this.scene, 16, 8);
+        this.sphere1 = new MySphere(this.scene, 16, 8);
+        this.elipse = new MyElipse(this.scene,16,8);
+        this.elipse1 = new MyElipse(this.scene,16,8);
+    }
+
+    display()
+    {
+        this.scene.pushMatrix();
+        
+        this.scene.scale(0.7,0.7,6);
+        this.scene.rotate(90*Math.PI/180,1,0,0);
+
+        this.cilinder.display();
+
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+
+        this.scene.scale(0.7,0.7,0.7);
+        this.sphere.display();
+
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+
+        this.scene.scale(0.7,0.7,0.7);
+        this.scene.translate(0,0,8.5); 
+
+        this.sphere1.display();
+        
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+
+        this.scene.translate(0.9,0,0);
+        this.scene.scale(0.3,0.3,0.5);
+
+        this.elipse.display();
+        
+        this.scene.popMatrix();
+
+        this.scene.pushMatrix();
+
+        this.scene.translate(-0.9,0,0);
+        this.scene.scale(0.3,0.3,0.5);
+
+        this.elipse1.display();
+        
+        this.scene.popMatrix();
+	}
+}
